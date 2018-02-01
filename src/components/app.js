5a925d0c9ba5b5f940441abf83af9aee26841fd3
munens <kaumbuthom@yahoo.com>
2018-01-31 18:40:34 -0800

    first commit: react-redux starter kit

---
+++ src/components/app.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import _ from 'lodash';
+
+export default class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {}
+  }
+
+  render() {
+    return (<div>App works!</div>);
+  }
+}
